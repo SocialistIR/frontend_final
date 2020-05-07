102e318e0f5818aec1a7fee10dfd869ead3704cc
Shubhankar <shubhankar1995@gmail.com>
2020-05-07 16:08:28 +1000

    added the login components

---
+++ build/precache-manifest.d49634d307d22b340dd994e9c8e9a424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a3c3e41ba91fdc1ffaef3c393b1806",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a71e968a8141c06643e",
+    "url": "/static/css/main.1b578f19.chunk.css"
+  },
+  {
+    "revision": "a3c600bb855e2b31a864",
+    "url": "/static/js/2.ed4dd02f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.ed4dd02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a71e968a8141c06643e",
+    "url": "/static/js/main.eee0b4a6.chunk.js"
+  },
+  {
+    "revision": "53cb442fe5926763f4bd",
+    "url": "/static/js/runtime-main.d2ba7e08.js"
+  }
+]);
